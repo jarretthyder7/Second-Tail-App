2e6d5323e920828f6ef4e3a9bb9cb53e04de1c1e
jarretthyder7 <jarretthyder7@gmail.com>
2026-04-20 22:57:37 -0400

    Rewrite /rescuespinner to static landing page

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
- 
+  async rewrites() {
+    return [
+      { source: '/rescuespinner', destination: '/rescuespinner/index.html' },
+    ]
+  },
 }
 
 export default nextConfig
